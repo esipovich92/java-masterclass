04775c71725acb2d5fbc250102d9b93c7bc9a1b1
Artem Esipovich <esipovich92@gmail.com>
2018-03-22 21:05:59 +0300

    Section 7 - OOP - classes

---
+++ src/com/esipovich/masterclass/classes/Car.java
@@ -0,0 +1,15 @@
+package com.esipovich.masterclass.classes;
+
+/**
+ * @author Artem Esipovich 22.03.2018
+ */
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String coloru;
+
+}
---
+++ src/com/esipovich/masterclass/classes/Main.java
@@ -0,0 +1,13 @@
+package com.esipovich.masterclass.classes;
+
+/**
+ * @author Artem Esipovich 22.03.2018
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+    }
+}
