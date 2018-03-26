f984d94188ab9f2029ab7df81deb77a65ff652b4
Artem Esipovich <esipovich92@gmail.com>
2018-03-26 22:09:59 +0300

    Section 7, lectures 41-44 - OOP - classes, constructors, inheritance

---
+++ src/com/esipovich/masterclass/section7/inheritance/challenge/Vehicle.java
@@ -0,0 +1,68 @@
+package com.esipovich.masterclass.section7.inheritance.challenge;
+
+/**
+ * @author Artem Esipovich 26.03.2018
+ */
+
+public class Vehicle {
+
+    private String name;
+    private String size;
+
+    private int currentVelocity;
+    private int currentDirection;
+
+    public Vehicle(String name, String size) {
+        this.name = name;
+        this.size = size;
+        this.currentVelocity = 0;
+        this.currentDirection = 0;
+    }
+
+    public void steer(int direction) {
+        this.currentDirection += direction;
+        System.out.println("Vehicle.steer(): Steering at " + currentDirection + " degrees");
+    }
+
+    public void move(int velocity, int direction) {
+        this.currentVelocity = velocity;
+        this.currentDirection = direction;
+        System.out.println("Vehicle.move(): Moving at " + currentVelocity + " int direction " + currentDirection);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getCurrentVelocity() {
+        return currentVelocity;
+    }
+
+    public void setCurrentVelocity(int currentVelocity) {
+        this.currentVelocity = currentVelocity;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public void stop() {
+        this.currentVelocity = 0;
+    }
+}
