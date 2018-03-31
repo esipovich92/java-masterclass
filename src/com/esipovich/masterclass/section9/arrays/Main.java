4d33b805f328c768503e84e7a60c84606709ab91
esipovich92 <esipovich92@gmail.com>
2018-03-31 13:01:49 +0400

    Section 9, lectures 54-55 (arrays, bubble sort, Arrays.copy)

---
+++ src/com/esipovich/masterclass/section9/arrays/Main.java
@@ -0,0 +1,12 @@
+package com.esipovich.masterclass.section9.arrays;
+
+/**
+ * @author Artem Esipovich 31.03.2018
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
+}
