69ebd3e96fe33aeb6d8fc9f2478e747ec9f4b9fc
esipovich92 <esipovich92@gmail.com>
2018-04-10 20:21:43 +0400

    Section 9, lectures 104-111 (Collections challenge)

---
+++ src/com/esipovich/masterclass/adventure/Location.java
@@ -0,0 +1,37 @@
+package com.esipovich.masterclass.adventure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Artem Esipovich 10.04.2018
+ */
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<>();
+        this.exits.put("Q", 0);
+    }
+
+    public void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<>(exits);
+    }
+}
