1b6953622bccc2e7c817aedaab82a564c67b10f1
esipovich92 <esipovich92@gmail.com>
2018-04-11 15:36:11 +0400

    Section 13, lectures 114-117 (Equals and Hashcode challenge)

---
+++ src/com/esipovich/masterclass/hashcode/Dog.java
@@ -0,0 +1,31 @@
+package com.esipovich.masterclass.hashcode;
+
+/**
+ * @author Artem Esipovich 11.04.2018
+ */
+
+public class Dog {
+    private final String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+
+        if(obj instanceof Dog) {
+            String objName = ((Dog) obj).getName();
+            return this.name.equals(objName);
+        }
+
+        return false;
+    }
+}
---
+++ src/com/esipovich/masterclass/hashcode/Labrador.java
@@ -0,0 +1,26 @@
+package com.esipovich.masterclass.hashcode;
+
+/**
+ * @author Artem Esipovich 11.04.2018
+ */
+
+public class Labrador extends Dog {
+
+    public Labrador(String name) {
+        super(name);
+    }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        if(this == obj) {
+//            return true;
+//        }
+//
+//        if(obj instanceof Labrador) {
+//            String objName = ((Labrador) obj).getName();
+//            return this.getName().equals(objName);
+//        }
+//
+//        return false;
+//    }
+}
